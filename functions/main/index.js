02fcc0348509bd4f250f6ad82152cf8f796ec619
shivkanthb <shivkanthb@gmail.com>
2017-05-11 19:27:16 -0700

    💯

--- functions/main/index.js
+++ functions/main/index.js
@@ -15,18 +15,42 @@
 */
 
 var gemoji = require('gemoji');
+var imojiClient = new (require("imoji-node"))({
+        apiKey: process.env.imoji_key,
+        apiSecret: process.env.imoji_secret
+    });
+
+var getSticker = function(input, cb) {
+	var response = {};
+	imojiClient.random({
+    	query: input
+    })
+    .then(function (random) {
+        if(random.results.length>0)
+        {
+            var url = random.results[0].urls.png.thumb;
+            response.status = "SUCCESS";
+            response.url = url;
+        }
+        else
+        	response.message = "No results found";
+        cb(null, response);
+    })
+    .catch(function (err) {
+        console.error(err);
+        cb(err, null);
+    });
+}
 
 module.exports = (params, callback) => {
+	console.log("key :"+process.env.imoji_key);
 	var query = params.kwargs.query || "hello world";
 	var result = gemoji.unicode[query] || gemoji.name[query]; 
 	var response = {};
-	if(result) {
-		var emoji_text = result.name;
-		response.text = emoji_text;
-	}
-	else {
-		response.text = "No text 😞"
-	}
-	callback(null, response);
-  	
+	
+	if(result)
+    	getSticker(result.name, callback);
+    else
+    	getSticker(query, callback);
+
 };
