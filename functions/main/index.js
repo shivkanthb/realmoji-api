d4a14a3d0df7609e69eaba734346625e9fc2a690
shivkanthb <shivkanthb@gmail.com>
2017-05-13 10:41:07 -0700

    emojione support

--- functions/main/index.js
+++ functions/main/index.js
@@ -3,6 +3,7 @@ var imojiClient = new (require("imoji-node"))({
         apiKey: process.env.imoji_key,
         apiSecret: process.env.imoji_secret
     });
+var emojione = require('emojione');
 
 var getSticker = function(input, cb) {
 	var response = {};
@@ -26,11 +27,37 @@ var getSticker = function(input, cb) {
     });
 }
 
+var getImage = function(input, cb) {
+    if(!input) {
+        cb({"error":"query parameter empty"}, null);
+        return;
+    }
+
+    var response = {};
+    var output = emojione.toImage(input);
+    var str = output,
+    re = /\ssrc=(?:(?:"([^"]*)"))/i, // match src="<url>"
+    res = str.match(re);
+    if(!res) {
+        cb({"error":"No emoji found"}, null);
+        return;
+    }
+    var src = res[1]||res[2]||res[3];
+    response.status = "SUCCESS";
+    response.url = src;
+    cb(null, response);
+}
+
 module.exports = (params, callback) => {
 	var query = params.kwargs.query || "hello";
+    var toImage = params.kwargs.toImage || false;
+
+    if(toImage) {
+        getImage(query, callback);
+        return;
+    }
+
 	var result = gemoji.unicode[query] || gemoji.name[query]; 
-	var response = {};
-	
 	if(result)
     	getSticker(result.name, callback);
     else
